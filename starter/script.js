b3533457082d2b2dded8c4537ef3f1b3c1f0fb70
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-09-23 21:25:41 +0100

    Arrow Functions part I

--- starter/script.js
+++ starter/script.js
@@ -6,6 +6,7 @@
 
 //ES5
 
+/*
 var name5 = 'Rodolfo Carvajal';
 var age5 = 31;
 name5 = 'Rodolfo Marquez';
@@ -34,6 +35,7 @@ function driverLicence5(passedTest){
     
     console.log(firstName + ', born in ' + yearOfBirth + '. ' + 'It is allow to drive!');
 }
+
 
 driverLicence5(true);
 
@@ -104,6 +106,7 @@ console.log(c);
 
 */
 
+/*
 /////////////////////////////////////////////////////////////
 // STRINGS
 /////////////////////////////////////////////////////////////
@@ -130,6 +133,33 @@ console.log(n.endsWith('l'));
 console.log(n.includes('arv'));
 console.log(`${firstName} `.repeat(5))
 
+*/
 
+/////////////////////////////////////////////////////////////
+// ARROW FUNCTIONS
+/////////////////////////////////////////////////////////////
 
+const years = [1987, 1989, 1999, 1975];
 
+// ES5
+
+var ages5 = years.map(function(current){
+    return 2019 - current;
+});
+
+console.log(ages5);
+
+// ES6
+
+let ages6 = years.map(current => 2019 - current);
+console.log(ages6);
+
+ages6 = years.map((current, index) => `Age elements ${index + 1}: ${2019 - current}.`);
+console.log(ages6);
+
+ages6 = years.map((current, index) => {
+    const now = new Date().getFullYear();
+    const age = now - current;
+    return `Age elements ${index + 1}: ${age}.`;
+})
+console.log(ages6);

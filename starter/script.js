2ca63b849becbf6d50f54943f8c67023e75a718a
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-09-24 22:27:16 +0100

    Arros Functions Part II

--- starter/script.js
+++ starter/script.js
@@ -133,7 +133,7 @@ console.log(n.endsWith('l'));
 console.log(n.includes('arv'));
 console.log(`${firstName} `.repeat(5))
 
-*/
+
 
 /////////////////////////////////////////////////////////////
 // ARROW FUNCTIONS
@@ -163,3 +163,112 @@ ages6 = years.map((current, index) => {
     return `Age elements ${index + 1}: ${age}.`;
 })
 console.log(ages6);
+
+*/
+
+
+/////////////////////////////////////////////////////////////
+// ARROW FUNCTIONS II
+/////////////////////////////////////////////////////////////
+
+
+// ES5
+
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function(){
+        
+        // TRICK - THIS IN DOCUMENT... REFERS TO THE BROWER WINDOWS
+        var self = this;
+        
+        document.querySelector('.green').addEventListener('click', function(){
+            var str = 'This is box number: ' + self.position + ' ans it is ' + self.color;
+            alert(str);
+        });
+    }
+    
+};
+
+//box5.clickMe();
+
+
+// ES6
+
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function(){
+                    
+        document.querySelector('.green').addEventListener('click', () => {
+            var str = 'This is box number: ' + this.position + ' ans it is ' + this.color;
+            alert(str);
+        });
+    }
+    
+};
+
+//box6.clickMe();
+
+
+/*
+
+const box66 = {
+    color: 'green',
+    position: 1,
+    clickMe: () => {
+                    
+        document.querySelector('.green').addEventListener('click', () => {
+            var str = 'This is box number: ' + this.position + ' ans it is ' + this.color;
+            alert(str);
+        });
+    }
+    
+};
+
+box66.clickMe();
+
+*/
+
+/*
+
+function Person(name) {
+    this.name = name;
+    
+};
+
+// ES5
+
+Person.prototype.myFriends5 = function(friends){
+            
+    var arr = friends.map(function(el){
+       return this.name + ' is friends with ' + el;                
+    }.bind(this));
+    
+    console.log(arr);
+};
+
+var friends = ['Juan', 'Carlos', 'Evin', 'Eva'];
+new Person('John').myFriends5(friends);
+
+// ES6
+
+Person.prototype.myFriends6 = function(friends){
+            
+    var arr = friends.map(el => {
+       return this.name + ' is friends with ' + el;                
+    });
+    
+    console.log(arr);
+};
+
+var friends = ['Juan', 'Carlos', 'Evin', 'Eva'];
+new Person('Mike').myFriends6(friends);
+
+*/
+
+
+
+
+
+
